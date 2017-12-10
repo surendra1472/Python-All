71af560b84cac8d58e48abd78d7b0c1bb30f87c5
Gandham <gandhas@amazon.com>
2017-12-10 10:52:49 +0530

    First Commit

---
+++ Explore/src/introduction/HelloWorld.py
@@ -0,0 +1,22 @@
+'''
+Created on Dec 10, 2017
+
+@author: gandhas
+'''
+
+class MyClass(object):
+    '''
+    First Class
+    '''
+
+
+    def __init__(self, name):
+        '''
+        Wish the world
+        '''
+        print("Hello %s !"%(name))
+
+
+if (__name__=="__main__"):
+    myClass = MyClass("Surendra")
+        
